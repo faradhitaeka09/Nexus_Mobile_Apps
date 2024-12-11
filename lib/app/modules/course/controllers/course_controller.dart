f4ddc7c27b41198174623bdc37a48c54fb205160
faradhitaeka09 <faradhita.eka.septiana@gmail.com>
2024-12-11 19:19:51 +0700

    first commit

---
+++ lib/app/modules/course/controllers/course_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class CourseController extends GetxController {
+  //TODO: Implement SettingController
+
+  final count = 0.obs;
+
+  void increment() => count.value++;
+}
